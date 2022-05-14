5482eacdb285b24dde28b2aaa2ffed633d76a9f0
yasinosman <yasinosman10@gmail.com>
2022-05-14 20:52:37 +0300

    chore: add datetime picker to project

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import { CssBaseline } from "@mui/material";
 import AuthenticationProvider, { ProtectedRoute } from "./context/Authentication";
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+import { LocalizationProvider } from "@mui/x-date-pickers";
 
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import HomePage from "./pages/Home";
@@ -7,50 +9,62 @@ import AccountPage from "./pages/Account";
 import LoginPage from "./pages/Login";
 import AddNewsPage from "./pages/AddNews";
 import AddEventPage from "./pages/AddEvent";
+import CoursesPage from "./pages/Courses";
 
 function App() {
 	return (
-		<AuthenticationProvider>
-			<BrowserRouter>
-				<CssBaseline />
-				<Routes>
-					<Route
-						path="/"
-						element={
-							<ProtectedRoute>
-								<HomePage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/addNews"
-						element={
-							<ProtectedRoute>
-								<AddNewsPage />
-							</ProtectedRoute>
-						}
-					/>
-					<Route
-						path="/addEvent"
-						element={
-							<ProtectedRoute>
-								<AddEventPage />
-							</ProtectedRoute>
-						}
-					/>
+		<LocalizationProvider dateAdapter={AdapterDateFns}>
+			<AuthenticationProvider>
+				<BrowserRouter>
+					<CssBaseline />
+					<Routes>
+						<Route
+							path="/"
+							element={
+								<ProtectedRoute>
+									<HomePage />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/addNews"
+							element={
+								<ProtectedRoute>
+									<AddNewsPage />
+								</ProtectedRoute>
+							}
+						/>
+						<Route
+							path="/addEvent"
+							element={
+								<ProtectedRoute>
+									<AddEventPage />
+								</ProtectedRoute>
+							}
+						/>
 
-					<Route path="/login" element={<LoginPage />} />
-					<Route
-						path="account"
-						element={
-							<ProtectedRoute>
-								<AccountPage />
-							</ProtectedRoute>
-						}
-					/>
-				</Routes>
-			</BrowserRouter>
-		</AuthenticationProvider>
+						<Route
+							path="/courses"
+							element={
+								<ProtectedRoute>
+									<CoursesPage />
+								</ProtectedRoute>
+							}
+						/>
+
+						<Route path="/login" element={<LoginPage />} />
+						<Route
+							path="account"
+							element={
+								<ProtectedRoute>
+									<AccountPage />
+								</ProtectedRoute>
+							}
+						/>
+					</Routes>
+				</BrowserRouter>
+			</AuthenticationProvider>
+		</LocalizationProvider>
 	);
 }
 
