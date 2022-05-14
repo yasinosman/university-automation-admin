939949cca850e430d2a8d752a7643fd08f7e49b8
yasinosman <yasinosman10@gmail.com>
2022-05-14 15:50:21 +0300

    feat(News): add add news form

--- src/pages/AddNews/AddNewsPage.jsx
+++ src/pages/AddNews/AddNewsPage.jsx
@@ -1,7 +1,8 @@
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import React from "react";
 import Navbar from "../../components/Navbar";
 import PageTitle from "../../components/PageTitle";
+import AddNewsForm from "./components/AddNewsForm";
 
 const AddNewsPage = () => {
 	return (
@@ -10,6 +11,10 @@ const AddNewsPage = () => {
 
 			<Container maxWidth="xl">
 				<PageTitle title="Haber Ekle" />
+			</Container>
+
+			<Container maxWidth="xl">
+				<AddNewsForm />
 			</Container>
 		</>
 	);
